779fb624497dffb145b034c9abc96c1db8afd320
okinyoCoder <okinyoanthony@gmail.com>
2025-07-26 18:02:45 +0300

    updates file

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -13,9 +13,9 @@ def with_db_connection(func):
 
 @with_db_connection 
 def get_user_by_id(conn, user_id): 
-cursor = conn.cursor() 
-cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
-return cursor.fetchone() 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
+    return cursor.fetchone() 
 #### Fetch user by ID with automatic connection handling 
 
 user = get_user_by_id(user_id=1)
--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -29,8 +29,8 @@ def transactional(func):
 @with_db_connection 
 @transactional 
 def update_user_email(conn, user_id, new_email): 
-cursor = conn.cursor() 
-cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
 #### Update user's email with automatic transaction handling 
 
 update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
