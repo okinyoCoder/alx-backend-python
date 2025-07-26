0d5be8cb8c1790dbcbb8fcc07b0f290588f80c1e
okinyoCoder <okinyoanthony@gmail.com>
2025-07-27 00:16:15 +0300

    Define data Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
+    created_at =models.DateField(auto_now_add=True)
+
+    REQUIRED_FIELDS = [ 'email', 'username']
+    USERNAME_FIELD = ['email']
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+    message_body = models.TextField()
+    sent_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} at {self.sent_at}"
+    
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    participants_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="conversations")
+    created_at = models.DateField(auto_now_add=True)
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -72,6 +72,15 @@ TEMPLATES = [
         },
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
